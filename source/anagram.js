2efd1e72cddd0f0b10d0f9e8fc1c7fc671c130cb
Meganster <ppro100takk@mail.ru>
2018-02-04 20:10:02 +0300

    HW version 1

---
+++ source/anagram.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const anagram = function (array) {
+    // отсортируем входной массив по алфавиту
+    array.sort();
+    
+    // будет хранить массивы слов
+    // которые являются анаграммами
+    let anagramsObject = {};
+
+    for (let i = 0; i < array.length; i++) {
+        // sortedWord будет хранить упорядоченный регистронезависимый 
+        // набор букв из которых состоит слово
+        // для этого разбиваем слово на буквы, сортируем и соединяем все обратно
+        let sortedWord = array[i].toLowerCase().split('').sort().join('');
+    
+        // создаст массив для ключа sortedWord 
+        // если он еще не был создан
+        if (anagramsObject[sortedWord] == undefined)
+            anagramsObject[sortedWord] = [];
+
+        // добавляем новое слово в массив
+        anagramsObject[sortedWord].push(array[i]);
+    }
+
+    let resultArr = [];
+    // теперь в anagramsObject для каждого ключа
+    // находится массив анаграмм 
+    for (let key in anagramsObject)
+        // отсеем все массивы в которых ровно 1 слово
+        // т.е. нет анаграмм
+        if (anagramsObject[key].length > 1)
+            resultArr.push(anagramsObject[key]);
+
+	return resultArr;
+}
